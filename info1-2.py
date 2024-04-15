d0a4b5ecf6e1c39ed3453ae34e9ec03aaaadf8db
SebastianMB <arancibiaelisa99@gmail.com>
2024-04-15 18:54:24 +0000

    Archivos de la evaluacion 1

---
+++ info1-2.py
@@ -0,0 +1,23 @@
+def main():
+
+    nombre = input("ingresar nombre: ")
+    apellido = input("ingresar apellido: ")
+    carrera = input("ingresar carrera: ")
+    asignatura = input("ingresa asignatura: ")
+    sede = input("ingresar sede: ")
+    semestre = input("ingresar semestre: ")
+    año = input("ingresar año actual: ")
+    ciudad = input("ingresar ciudad: ")
+    edad = input("ingresar edad: ")
+    
+    print(f"""el nombre y apellido del alumno es {nombre} {apellido}, estudia la carrera de {carrera}
+          cursando la asignatura de {asignatura},en la sede de {sede}, en el {semestre} semestre,
+          en el año {año}, en la ciudad de {ciudad},con una edad de {edad}años""")
+    
+if __name__ == "__main__":
+   main()
+    
+
+    
+
+
